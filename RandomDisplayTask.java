063e84f62b8d5e2aa6c5b903d4306b5bf6de7581
Jeff <jeff.uca@gmail.com>
2020-02-24 20:58:39 +0100

    [ADD] processing in task: painting points [ADD] initialize logic

--- RandomDisplayTask.java
+++ RandomDisplayTask.java
@@ -8,25 +8,30 @@
 
 public class RandomDisplayTask
 {
-
     private static int[][] matrix;
-
     private static Canvas canvasTemplateRef;
+    private static EngineGenerator handler = new EngineGenerator();
+    private static RandomGenerator rg ;
+    private static String random_engine;
+    private static int seq_len;
+    private static int seed;
 
     public int[][] getData() { return matrix; }
 
     public void plug(Canvas ref) { canvasTemplateRef = ref; }
 
-    public void initializer(int value) {
+    public void initializer(int seed, int sequence_length, String random_engine) {
         matrix = new int[1000][1000];
 
-        for (int i = 0; i < 1000 ; i++) {
-            for (int j = 0; j < 1000 ; j++) {
-                matrix[i][j] = value;
+        for (int i = 0; i < 1000 ; i++)
+            for (int j = 0; j < 1000 ; j++)
+                matrix[i][j] = 0;
 
-            }
-
-        }
+        handler.createEngines();
+        rg = new RandomGenerator(seed);
+        RandomDisplayTask.random_engine = random_engine;
+        seq_len = sequence_length;
+        RandomDisplayTask.seed = seed;
     }
     public static Boolean abort = false;
 
@@ -34,40 +39,25 @@ public class RandomDisplayTask
         abort = true;
     }
 
-
-
-    public void computeClassNV(int line)
-    {
+    public void computeClassNV(int color) {
 
         abort = false;
+        int point_value = 0;
+        int random_generated;
+        rg.reset();
 
-
-
-        if(matrix[0][0]==2)
-        {
-            for (int j = 1; j < 1000; j++ ) {
-                if(abort)
-                    break;
-                matrix[line][j] = 1;
+        for(int i = 1; i < seq_len; i++){
+            if(abort)
+                break;
+            random_generated = rg.getIthRandomNumber(handler.engines.get(random_engine), seed,i).intValue()%1000;
+            System.out.println(random_generated);
+            if(i%2==1)
+                point_value= random_generated%1000;
+            else{
+                matrix[point_value][random_generated]=color;
                 canvasTemplateRef.paintImmediately(0,0,1000,1000);
-
-            }
-
-        }
-        else if(matrix[0][0]==3){
-            for (int j = 1; j < 1000; j++ ) {
-                if(abort)
-                    break;
-                matrix[j][line] = 1;
-                canvasTemplateRef.paintImmediately(0,0,1000,1000);
-
             }
         }
-
-
-
-
-
     }
     
 }
